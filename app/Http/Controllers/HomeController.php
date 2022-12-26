d8922de8ced4c9e6f042aa0e4c043f990473dd7e
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-12-26 16:28:02 +0100

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Mail;
-use App\Mail\ContactFormMail;
-use Illuminate\Contracts\Mail\Mailable;
+use App\Models\Contact;
 
 class HomeController extends Controller
 {
@@ -31,12 +29,8 @@ class HomeController extends Controller
                 'email' => $request['email'],
                 'message' => $request['message'],
             ];
-            Mail::send('Html.view', $contact, function ($message) {
-                $message->from('noreply@management.virtualprovision.ma', 'Portfolio');
-                $message->sender('noreply@management.virtualprovision.ma', 'Portfolio');
-                $message->to('chaibiimranA@gmail.com', 'Portfolio');
-                $message->subject('Subject');
-            });
+            // saving the message to the database
+            Contact::create($contact);
             return redirect()->back()->with('toast_success', 'Your message has been sent successfully');
         }
     }
